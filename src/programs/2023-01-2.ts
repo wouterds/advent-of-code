b1aada73b0756eadbdf94acfc6fe5527ac466f84
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-02 14:30:30 +0100

    feat: day 1, part 2

---
+++ src/programs/2023-01-2.ts
@@ -0,0 +1,95 @@
+import chalk from 'chalk';
+
+import { AbstractProgram } from '../abstracts/program';
+import { IProgram } from '../interfaces/program';
+
+class Program extends AbstractProgram implements IProgram {
+  private constructor() {
+    super('2023-01-input.txt');
+  }
+
+  private wordNumberMap = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  private findFirstNumber(string: string): number {
+    let number = 0;
+    let lowestIndex = Infinity;
+    for (const word of Object.keys(this.wordNumberMap)) {
+      const index = string.indexOf(word);
+      if (index === -1) {
+        continue;
+      }
+
+      if (index < lowestIndex) {
+        lowestIndex = index;
+        number = this.wordNumberMap[word];
+      }
+    }
+
+    const decimalNumberMatch = string.match(/\d/g)?.shift?.();
+    if (decimalNumberMatch) {
+      const index = string.indexOf(decimalNumberMatch);
+      if (index < lowestIndex) {
+        lowestIndex = index;
+        number = parseInt(decimalNumberMatch);
+      }
+    }
+
+    return number;
+  }
+
+  private findLastNumber(string: string): number {
+    let number = 0;
+    let highestIndex = -1;
+    for (const word of Object.keys(this.wordNumberMap)) {
+      const indexes = string.matchAll(new RegExp(word, 'g'));
+      for (const index of indexes) {
+        if (index.index > highestIndex) {
+          highestIndex = index.index;
+          number = this.wordNumberMap[word];
+        }
+      }
+    }
+
+    const decimalNumberMatch = string.match(/\d/g)?.pop?.();
+    if (decimalNumberMatch) {
+      const index = string.lastIndexOf(decimalNumberMatch);
+      if (index > highestIndex) {
+        highestIndex = index;
+        number = parseInt(decimalNumberMatch);
+      }
+    }
+
+    return number;
+  }
+
+  public async run() {
+    const lines = this.data.split('\n');
+
+    let total = 0;
+    for (const line of lines) {
+      process.stdout.write(`[${chalk.yellow(total)}] ${line}`);
+
+      const firstNumber = this.findFirstNumber(line);
+      const lastNumber = this.findLastNumber(line);
+
+      const number = `${firstNumber}${lastNumber}`;
+      console.log(chalk.green(` ${number}`));
+
+      total += parseInt(number);
+    }
+
+    this.solution = total.toString();
+  }
+}
+
+export default Program;
